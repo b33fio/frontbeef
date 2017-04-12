190db0afbe7c0869e376033d7ae6b5858475d2bb
Justin Ledford <justinledford@gmail.com>
2017-04-12 18:09:07 -0500

    generate browser module and components

---
+++ src/app/browser/browser.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { GlobalBrowserComponent } from './global-browser/global-browser.component';
+import { ChannelBrowserComponent } from './channel-browser/channel-browser.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [GlobalBrowserComponent, ChannelBrowserComponent]
+})
+export class BrowserModule { }
