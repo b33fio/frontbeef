0761abd1fa6f92bceb7678777f8b1ce8eefd0260
Lucas Hansen <respun1000@gmail.com>
2017-04-13 09:36:48 -0500

    Added home page and routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,47 @@ import { BeefDebateModule } from './beef-debate/beef-debate.module'
 import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './beef-landing/home-page/home-page.component'
 
+const appRoutes: Routes = [
+  //landing page, handles login/sign up
+  {
+    path: '', 
+    component: HomePageComponent
+  },
+
+  //browse top debates
+  { path: 'browse', 
+    component: HomePageComponent
+  },
+
+  //browse debates within a given (pre-defined channels)
+  // { path: 'browse/:channel',
+  //   component: AppComponent
+  // },
+
+  //look at a debate (open/in-progres/closed as spectator/participant/public)
+  // { path: 'debate/:id',
+  //   component: AppComponent
+  // },
+
+  //view account settings and edit
+  // { path: 'account',
+  //   component: AppComponent
+  // },
+
+  //any other route gets redirected to a PageNotFound component
+  { path: '**', 
+   component: HomePageComponent
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
     FormsModule,
     HttpModule,
--- src/app/beef-landing/beef-landing.module.ts
+++ src/app/beef-landing/beef-landing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [HomePageComponent]
 })
 export class BeefLandingModule { }
