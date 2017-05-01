1daa64cffc0d5a15b4219596391bb1fddb2703e9
Lucas Hansen <respun1000@gmail.com>
2017-05-01 14:34:21 -0500

    hotfix2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ import { User } from './beef-api/classes/user'
 export class AppComponent {
   title = 'Beef Master Test';
   user:User;
-  constructor(private beefApiService: BeefApiService,private router: Router,private route: ActivatedRoute){};
+  constructor(public beefApiService: BeefApiService,private router: Router,private route: ActivatedRoute){};
   ngOnInit() {
     //this.ngAfterViewChecked()
     this.user = this.beefApiService.getUser();
